9e4a54087fd230a09014cfbe96dba8746e3eb475
amal francis <amalfrancis744@gmail.com>
2023-11-13 02:27:46 +0530

    chat working properly

--- clint/src/components/userAvatar/SingleChat.jsx
+++ clint/src/components/userAvatar/SingleChat.jsx
@@ -7,6 +7,23 @@ import { getSender, getSenderFull } from '../../config/ChatLogics';
 import ProfileModal from '../miscellaneous/profileModal';
 import UpdateGroupChatModal from '../miscellaneous/UpdateGroupChatModal';
 import axios from 'axios';
+import { useEffect } from 'react';
+import "./styles.css"
+import ScrollableChat from './ScrollableChat';
+import io from 'socket.io-client'
+import Lottie from "react-lottie";
+import animationData from "../animations/typing.json"
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+const ENDPOINT = "http://localhost:8800"
+var socket,selectedChatCompare
 
 
 function SingleChat({ fetchAgain, setFetchAgain }) {
@@ -26,10 +43,55 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
      const { selectedChat, setSelectedChat, user, notification, setNotification } =
         ChatState();
 
+  const fetchMessages = async () => {
+    if (!selectedChat) return;
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      setLoading(true);
+
+      const { data } = await axios.get(
+        `http://localhost:8800/users/${selectedChat._id}`,
+        config
+      );
+      setMessages(data);
+        setLoading(false);
+          console.log("mesages form my",data)
+
+      socket.emit("join chat", selectedChat._id);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Messages",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+    }
+    };
+      useEffect(() => {
+
+        socket = io(ENDPOINT)
+        socket.emit("setup",user)
+          socket.on('connected', () => setSocketConnected(true))
+            socket.on("typing", () => setIsTyping(true));
+    socket.on("stop typing", () => setIsTyping(false));
+        
+
+    }, [])
+
 
     const sendMessage = async(event) => {
         if (event.key === "Enter" && newMessage)
         {
+              socket.emit("stop typing", selectedChat._id);
+
             try {
 
                  const config = {
@@ -47,8 +109,8 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
           config
                 );
                 
-
-      console.log("mesages form my",data)
+ socket.emit("new message", data);
+    
         setMessages([...messages, data]);
                
            }catch(error)
@@ -67,10 +129,51 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
         }
         
     }
-    const typingHandler = (e) => {
-        setNewMessage(e.target.value)
-        
+   const typingHandler = (e) => {
+    setNewMessage(e.target.value);
+
+    if (!socketConnected) return;
+
+    if (!typing) {
+      setTyping(true);
+      socket.emit("typing", selectedChat._id);
     }
+    let lastTypingTime = new Date().getTime();
+    var timerLength = 3000;
+    setTimeout(() => {
+      var timeNow = new Date().getTime();
+      var timeDiff = timeNow - lastTypingTime;
+      if (timeDiff >= timerLength && typing) {
+        socket.emit("stop typing", selectedChat._id);
+        setTyping(false);
+      }
+    }, timerLength);
+  };
+
+ useEffect(() => {
+    fetchMessages();
+
+    selectedChatCompare = selectedChat;
+    // eslint-disable-next-line
+  }, [selectedChat]);
+
+  
+    
+      useEffect(() => {
+    socket.on("message recieved", (newMessageRecieved) => {
+      if (
+        !selectedChatCompare || // if chat is not selected or doesn't match current chat
+        selectedChatCompare._id !== newMessageRecieved.chat._id
+      ) {
+        if (!notification.includes(newMessageRecieved)) {
+          setNotification([newMessageRecieved, ...notification]);
+          setFetchAgain(!fetchAgain);
+        }
+      } else {
+        setMessages([...messages, newMessageRecieved]);
+      }
+    });
+  });
     
   return (
       <>
@@ -99,7 +202,9 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
                               <>{selectedChat.chatName.toUpperCase()}
                                   <UpdateGroupChatModal
                                       fetchAgain={fetchAgain}
-                                      setFetchAgain={setFetchAgain} />
+                                      setFetchAgain={setFetchAgain}
+                                  fetchMessages={fetchMessages}
+                                  />
                               </>
                       )}
                       
@@ -124,15 +229,24 @@ function SingleChat({ fetchAgain, setFetchAgain }) {
                               margin="auto"
                           />
                       ) : (
-                          <>
-                            {/* messages */}
-                        </>
+                          <div className="messages">
+                <ScrollableChat messages={messages} />
+              </div>
                               
                 )}
 
-                     <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+                      <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+                          {istyping ? <div>
+                             <Lottie
+                    options={defaultOptions}
+                    // height={50}
+                    width={70}
+                    style={{ marginBottom: 15, marginLeft: 0 }}
+                  />
+                          </div>:<></>}
                                   
                           <Input
+                              
                               variant="filled"
                               bg="#E0E0E0"
                               placeholder='Enter a message..'
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import path from "path";
-
+import { Server } from 'socket.io';
 // security packges
 import helmet from "helmet";
 import dbConnection from "./dbConfig/index.js";
@@ -33,6 +33,53 @@ app.use(morgan("dev"));
 app.use(router);
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
+const server= app.listen(PORT, () => {
   console.log(`Server running on port:${PORT}`);
 });
+
+
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io")
+  socket.on('setup', (userData) => {
+    
+    socket.join(userData._id)
+    console.log(userData._id)
+
+    socket.emit('connected')
+  });
+
+    socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+    });
+  
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+    socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+
+
+})
